f4d4c33b870da897406924e1140e33a78d54d366
Alen Čamagajevac <alen.camagajevac@gmail.com>
2018-01-31 21:13:51 +0100

    Added redux boilerplate

---
+++ src/Router.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Router } from 'react-native-router-flux';
+
+const RouterComponent = () => {
+    return (
+        <Router />
+    );
+};
+
+export default RouterComponent;
