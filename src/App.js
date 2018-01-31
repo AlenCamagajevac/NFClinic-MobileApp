924ac9b8f7c13758d4b3cfbf2a9ac82fdbffcd29
Alen Čamagajevac <alen.camagajevac@gmail.com>
2018-01-31 21:34:32 +0100

    wired up the redux-saga to middleware

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import Router from './Router';
 
 class App extends Component {
     render() {
-        const store = createStore(reducers, {}, applyMiddleware());
+        const store = createStore(reducers, {}, applyMiddleware(createSagaMiddleware()));
 
         return (
             <Provider store={store}>
